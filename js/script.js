094e700733a3b9a78a711ea56348824fed3e3e39
Isaac Herrera <isaacherrera47@gmail.com>
2017-05-14 19:26:36 -0700

    + added main form

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(".button-collapse").sideNav();
 $('select').material_select();
-$('.chips').material_chip();
+$('.chips').material_chip({
+    secondaryPlaceholder: '+Msisdn'
+});
 $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
